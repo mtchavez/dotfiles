b7f6042cf1149cc017bbffaccfa9aa47b8c4c48c
Chavez <matthew@el-chavez.me>
2017-06-12 21:37:30 -0700

    Add a provision flag to avoid a resetting of bash profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
+
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
+
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "*.un~" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "*.un~" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
+  reset
+elif [ "$1" == "--provision" -o "$1" == "-p" ]; then
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-        chmod 644 $HOME/.ssh/config*
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+    reset
+    chmod 644 $HOME/.ssh/config*
+  fi
 fi
 unset doIt
